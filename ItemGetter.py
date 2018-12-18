4e6f472fef90179d026400426e6ac7c6f5d50f6d
RandieBarsteward <39021056+RandieBarsteward@users.noreply.github.com>
2018-12-18 15:08:22 +0000

    Update ItemGetter.py

--- ItemGetter.py
+++ ItemGetter.py
@@ -1,8 +1,8 @@
 import operator
 
-cats = ['frank', 'Ron', 'Jess', 'Phil']
+cats = [['frank',14,56.5], ['Ron',8,70], ['Jess',7,0], ['Phil',3,150]]
 
-itemsorter = operator.itemgetter(0)
+itemsorter = operator.itemgetter(1)
 cats.sort(key=itemsorter)
 
 print(cats)
