0a017a21b60bcea76d0de10ed875ce7e7cb21db3
RandieBarsteward <39021056+RandieBarsteward@users.noreply.github.com>
2018-12-18 15:45:05 +0000

    Create ClassDelegate.py

---
+++ ClassDelegate.py
@@ -0,0 +1,40 @@
+class delegate(object):
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+        self.__results = []
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def set_age(self, age):
+        pass
+
+    def get_age(self):
+        return self.__age
+
+    def set_results(self):
+        pass
+
+    def output_results(self):
+        pass
+
+#Create a new instance of delegate called newDelegate (object)
+newDelegate = delegate("Frank",14)
+print(newDelegate.get_name()) #takes name from get_name method
+#print(newDelegate.__name) # throw error because name is private to the class
+print(newDelegate.get_age()) #takes name from get_age method
+
+newDelegate2 = delegate("Bobby",12)
+print(newDelegate2.get_name()) #takes name from get_name method
+#print(newDelegate2.__name) # throw error because name is private to the class
+print(newDelegate2.get_age()) #takes name from get_age method
+
+newDelegate.set_name("Franklin") #calls setname to change the value of name
+print(newDelegate.get_name()) #takes name from get_name method
+#print(newDelegate.__name) # throw error because name is private to the class
+print(newDelegate.get_age()) #takes name from get_age method
+
